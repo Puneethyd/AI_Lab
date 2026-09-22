d0f50b6f3e72fc10eca1022cf62f56254d6fb540
Puneethyd <puneethyd.cs24@bmsce.ac.in>
2026-09-22 16:29:03 +0530

    Update python_basics.py

--- python_basics.py
+++ python_basics.py
@@ -1,8 +1,15 @@
-age = 25
-height = 1.80  
-weight = 75  
+height = 1.80
+weight = 75
 
 bmi = weight / (height**2)
-print(
-    f"Your age is {age}, weight is {weight} kg, and height is {height} m, so your BMI is {bmi:.2f}"
-)
+print(f"Your BMI is {bmi:.2f}")
+
+
+if bmi < 18.5:
+    print("You fall into the Underweight category.")
+elif bmi <= 22.9:
+    print("You fall into the Normal weight category.")
+elif bmi <= 27.4:
+    print("You fall into the Overweight category.")
+else:
+    print("You fall into the Obese category.")
